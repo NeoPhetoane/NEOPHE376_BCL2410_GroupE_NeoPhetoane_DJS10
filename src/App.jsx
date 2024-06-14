38b3d9177bfe913883c9bb6dc0496cf3275b5bcd
NeoPhetoane <phetoaneneo4@gmail.com>
2024-06-14 16:52:23 +0200

    Comments added to explain the code choices.

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
-import { useState, useEffect } from "react";
+//Imports react in order to use it.
+import React, { useState, useEffect } from "react";
 
 function App() {
+
+  //state initialised to store the posts and error message
   const [posts, setPosts] = useState([]);
   const [error, setError] = useState(null);
 
+//Function that fetches data from the API
   useEffect(() => {
     const fetchPostsFromApi = async () => {
       try {
@@ -14,24 +18,29 @@ function App() {
           throw new Error("Data fetching failed.");
         }
         const data = await response.json();
-        setPosts(data);
+
+        setPosts(data); // Updates state with fetched posts
       } catch (error) {
-        setError(error.message);
+        setError(error.message); //Updates state with error message
       }
     };
 
     fetchPostsFromApi();
-  }, []);
+  }, []);  //  Empty dependency array
 
   return (
     <>
       <h1>Posts</h1>
+      {/* Ternary operator that manages what displays on the page */}
+
+
       {error ? (
         <div className="error-message">
           <p>Error: {error}</p>
         </div>
       ) : (
         <ol>
+          {/* Itterates over the API data and looks within to find the id, title and body */}
         {posts.map((post) => (
           <li key={post.id}>
             <h2>{post.title}</h2>
@@ -44,4 +53,5 @@ function App() {
   );
 }
 
+//Exporting the function to main so it can display
 export default App;
